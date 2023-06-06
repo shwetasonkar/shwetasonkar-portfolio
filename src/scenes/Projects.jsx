8e36f2fd9265dff8eb9b2936ef3feb30f9513b1f
shwetasonkar <shwetasonkar505@gmail.com>
2023-06-07 00:11:16 +0530

    Update code with new feature

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -16,7 +16,7 @@ const projectVariant = {
 };
 
 const Project = ({ title }) => {
-  const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
+  const overlayStyles = `absolute h-600 w-400 opacity-0 hover:opacity-90 transition duration-500
     bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
   const projectTitle = title.split(" ").join("-").toLowerCase();
 
